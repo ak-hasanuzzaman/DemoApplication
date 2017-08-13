7427a9edf309b5369cde1330c764fa1e461703af
ak-hasanuzzaman <hasan.se.bd@gmail.com>
2017-08-13 23:06:51 +0200

    5 more commits are merged

--- app/app.routing.js
+++ app/app.routing.js
@@ -2,14 +2,16 @@
 var login_component_1 = require("./login.component");
 var home_component_1 = require("./home.component");
 var router_1 = require("@angular/router");
+var authmanager_1 = require("./authmanager");
 var appRoutes = [
     {
         path: '',
         redirectTo: "Login",
         pathMatch: 'full'
     },
-    { path: 'Login', component: login_component_1.LoginComponent },
-    { path: 'Home', component: home_component_1.HomeComponent },
+    { path: 'Login', component: login_component_1.LoginComponent, canActivate: [authmanager_1.AuthManager] },
+    { path: 'Home', component: home_component_1.HomeComponent, canActivate: [authmanager_1.AuthManager] },
 ];
+exports.appRoutingProviders = [];
 exports.routing = router_1.RouterModule.forRoot(appRoutes);
 //# sourceMappingURL=app.routing.js.map
